3d50fd82904b91f6845b009efaa754fb8a02be9a
ashrafsaleemmonash <84313482+ashrafsaleemmonash@users.noreply.github.com>
2021-09-19 08:03:34 +1000

    Add project files.

---
+++ Assignment1FIT5032/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment1FIT5032.MvcApplication" Language="C#" %>
---
+++ Assignment1FIT5032/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment1FIT5032.Startup))]
+namespace Assignment1FIT5032
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
