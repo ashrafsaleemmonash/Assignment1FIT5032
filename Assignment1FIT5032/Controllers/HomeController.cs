1dd6b1ea674ea532da69f4523f06a9642aa14d02
ashrafsaleemmonash <84313482+ashrafsaleemmonash@users.noreply.github.com>
2021-09-24 08:17:08 +1000

    Adding SendGrid Through SMTP

--- Assignment1FIT5032/Controllers/HomeController.cs
+++ Assignment1FIT5032/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,5 +31,35 @@ namespace Assignment1FIT5032.Controllers
 
             return View();
         }
+
+        public ActionResult Email()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }   
+
+        [HttpPost]
+        public async Task<ActionResult> form1(string txtTo, string txtSubjectTitle, string txtContents)
+        {
+
+            SmtpSection smtpSection = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
+            using (MailMessage mm = new MailMessage(smtpSection.From, txtTo))
+            {
+                mm.Subject = txtSubjectTitle;
+                mm.Body = txtContents;
+                mm.IsBodyHtml = false;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = smtpSection.Network.Host;
+                smtp.EnableSsl = smtpSection.Network.EnableSsl;
+                NetworkCredential networkCred = new NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password);
+                smtp.UseDefaultCredentials = smtpSection.Network.DefaultCredentials;
+                smtp.Credentials = networkCred;
+                smtp.Port = smtpSection.Network.Port;
+                smtp.Send(mm);
+            }
+            return View("Email");
+        }
+
     }
 }
