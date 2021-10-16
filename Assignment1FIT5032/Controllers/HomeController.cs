83abeba7de0133ad3f406c7ada4b0c7749047c8d
ashrafsaleemmonash <84313482+ashrafsaleemmonash@users.noreply.github.com>
2021-10-17 10:08:51 +1100

    Added Bulk Email

--- Assignment1FIT5032/Controllers/HomeController.cs
+++ Assignment1FIT5032/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Assignment1FIT5032.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -11,12 +14,13 @@ using System.Net.Mime;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-
+ 
 namespace Assignment1FIT5032.Controllers
 {
     [RequireHttps]
     public class HomeController : Controller
     {
+        private ApplicationUserManager _userManager;
         public ActionResult Index()
         {
             return View();
@@ -46,35 +50,73 @@ namespace Assignment1FIT5032.Controllers
         // Email
         [HttpPost]
         //string txtTo, string txtSubjectTitle, string txtContents,
-        public async Task<ActionResult> form1(string txtTo, string txtSubjectTitle, string txtContents, HttpPostedFileBase fileAttachment)
+        public async Task<ActionResult> form1(string txtTo, string txtSubjectTitle, string txtContents, HttpPostedFileBase fileAttachment, bool bulkEmail)
         {
-            System.Diagnostics.Debug.WriteLine("SomeText");
-            System.Diagnostics.Debug.WriteLine(fileAttachment.FileName);
-
 
             SmtpSection smtpSection = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
-            using (MailMessage mm = new MailMessage(smtpSection.From, txtTo))
+            if (bulkEmail)
             {
-                mm.Subject = txtSubjectTitle;
-                mm.Body = txtContents;
-                mm.IsBodyHtml = false;
+                _userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var users = _userManager.Users;
+               
+                using (MailMessage mm = new MailMessage())
+                {
 
-                Attachment data = new Attachment(
-                fileAttachment.InputStream,
-                fileAttachment.FileName);
+                    foreach (var user in users)
+                    {
+                        mm.To.Add(user.Email);
+                    }
+                    mm.From = new MailAddress(smtpSection.From);
+                    mm.Subject = txtSubjectTitle;
+                    mm.Body = txtContents;
+                    mm.IsBodyHtml = false;
 
-                mm.Attachments.Add(data);
+                    
+                    Attachment data = new Attachment(
+                    fileAttachment.InputStream,
+                    fileAttachment.FileName);
 
-                SmtpClient smtp = new SmtpClient();
-                smtp.Host = smtpSection.Network.Host;
-                smtp.EnableSsl = smtpSection.Network.EnableSsl;
-                NetworkCredential networkCred = new NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password);
-                smtp.UseDefaultCredentials = smtpSection.Network.DefaultCredentials;
-                smtp.Credentials = networkCred;
-                smtp.Port = smtpSection.Network.Port;
-                smtp.Send(mm);
+                    mm.Attachments.Add(data);
+
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = smtpSection.Network.Host;
+                    smtp.EnableSsl = smtpSection.Network.EnableSsl;
+                    NetworkCredential networkCred = new NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password);
+                    smtp.UseDefaultCredentials = smtpSection.Network.DefaultCredentials;
+                    smtp.Credentials = networkCred;
+                    smtp.Port = smtpSection.Network.Port;
+                    smtp.Send(mm);
+
+                }
 
             }
+            else
+            {
+                using (MailMessage mm = new MailMessage(smtpSection.From, txtTo))
+                {
+
+                    mm.Subject = txtSubjectTitle;
+                    mm.Body = txtContents;
+                    mm.IsBodyHtml = false;
+
+                    Attachment data = new Attachment(
+                    fileAttachment.InputStream,
+                    fileAttachment.FileName);
+
+                    mm.Attachments.Add(data);
+
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = smtpSection.Network.Host;
+                    smtp.EnableSsl = smtpSection.Network.EnableSsl;
+                    NetworkCredential networkCred = new NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password);
+                    smtp.UseDefaultCredentials = smtpSection.Network.DefaultCredentials;
+                    smtp.Credentials = networkCred;
+                    smtp.Port = smtpSection.Network.Port;
+                    smtp.Send(mm);
+
+                }
+            }
+
             return View("Email");
         }
 
